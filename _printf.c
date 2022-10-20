bfd34f3b476e97725aff1d3cb8a4f52fa222f885
xjomino <jammwa@gmail.com>
2022-10-20 10:23:35 +0000

    test

--- _printf.c
+++ _printf.c
@@ -1,11 +1,8 @@
 #include "main.h"
-
 /**
  * _printf - prints formatted data to stdout
  * @format: string that contains the format to print
  * Return: number of characters written
  */
 int _printf(const char *format, ...);
-{
-	
-}
+{}
